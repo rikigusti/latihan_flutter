c3951a61c56aac45d18198e81b6ee08e99e8108b
rikigusti <soksaha226@gmail.com>
2023-02-27 07:43:16 +0700

    lupa step ke berapa

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,12 +1,30 @@
+import 'dart:async';
+
+// import 'package:app1/app/modules/dashboard/views/dashboard_view.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+import '../../dashboard/views/dashboard_view.dart';
+import '../../login/views/login_view.dart';
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
 
-  final count = 0.obs;
+  late Timer _pindah;
+  final authToken = GetStorage();
+
   @override
   void onInit() {
     super.onInit();
+    _pindah = Timer.periodic(
+      const Duration(seconds: 4),
+      (timer) => authToken.read('token') == null
+          ? Get.off(
+              () => const LoginView(),
+              transition: Transition.leftToRight,
+            )
+          : Get.off(() => const DashboardView()),
+    );
   }
 
   @override
@@ -17,7 +35,6 @@ class HomeController extends GetxController {
   @override
   void onClose() {
     super.onClose();
+    _pindah.cancel();
   }
-
-  void increment() => count.value++;
 }
